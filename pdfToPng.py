aa081ba5545ca5fc385fcea217286ac8e46b9eef
Multifactored <multifactored@gmail.com>
2021-01-13 15:55:33 +1100

    initial public facing commit

--- pdfToPng.py
+++ pdfToPng.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from pdf2image import convert_from_path, convert_from_bytes
+import sys
 
+fileName = sys.argv[1]
 
-pages = convert_from_bytes(open('resume.pdf', 'rb').read())
+pages = convert_from_bytes(open(fileName, 'rb').read())
 
 count = 0
 for page in pages:
